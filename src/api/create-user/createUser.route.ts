c0e4e9a8cfbed07b888a3df04aaf3537eca497f3
Jack <contact@jackhayes.tech>
2022-12-23 18:54:37 +1100

    Create user route.

---
+++ src/api/create-user/createUser.route.ts
@@ -0,0 +1,19 @@
+import { Request } from "express"
+import { ResponseData } from "@jh-tech/response-object"
+
+import { CreateUserRequest } from "../../data/interfaces"
+// import { createUser } from "./createUser.controller"
+
+/**
+ * Route for creating the create event
+ * @param req
+ * @param res
+ * @returns
+ */
+export const createUserRoute = async (req: Request<object, object, CreateUserRequest>, res: ResponseData) => {
+  const data = req.body.data
+
+  res.data = data
+
+  return
+}
--- src/data/interfaces/index.ts
+++ src/data/interfaces/index.ts
@@ -1,1 +1,3 @@
-export * from './user.interface'
+export * from "./user.interface"
+export * from "./requests.interface"
+export * from "./types"
