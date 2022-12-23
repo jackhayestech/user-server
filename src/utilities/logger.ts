b73b754dce53b8dcc3ca26a46ba1ce074763f077
Jack <contact@jackhayes.tech>
2022-12-23 18:36:58 +1100

    Created the utilities folder.

---
+++ src/utilities/logger.ts
@@ -0,0 +1,7 @@
+/**
+ * A logger function that can be expanded for custom logging.
+ * @param data - The data to be logged.
+ */
+export const logger = (data: unknown): void => {
+  console.log(data)
+}
